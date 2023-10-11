f2c29c8f1529b9336bf45d97983f4fd22e669ebd
Aguilar Daniel <124215580+DanielFrikel@users.noreply.github.com>
2023-10-11 11:35:20 -0700

    Add files via upload

---
+++ Sandbox/Testing2/Testing2/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing2
+{
+    interface IAnimal 
+    {
+        void animalSound();
+        void animalName();
+    }
+
+    interface ILegs 
+    {
+        void howManyLegs();
+    }
+
+    public class Cow : IAnimal, ILegs {
+        public void animalSound()
+        {
+            Console.WriteLine("The animal says: Muuu");
+        }
+        public void animalName()
+        {
+            Console.WriteLine("The animal is a Cow.");
+        }
+
+
+        public void howManyLegs()
+        {
+            Console.WriteLine("The Cow has 4 legs.");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 26;
+            //C# does not support "multiple inheritance"
+            //(a class can only inherit from one base class).
+            //However, it can be achieved with interfaces,
+            //because the class can implement multiple interfaces.
+            //Note: To implement multiple interfaces,
+            //separate them with a comma (see example below).
+            Cow cow = new Cow();
+            cow.animalSound();
+            cow.animalName();
+
+            cow.howManyLegs();            
+            Action anonymousFunction = () => Console.WriteLine("Funcion Anonima");
+            anonymousFunction();
+            
+            //<type of first parameter, type of second, output(type of return value)>
+            Func<int, int, int> multiply = (num1, num2) => num1 * num2;
+            Console.WriteLine(multiply(3, 3));            
+
+            Func<int, string> isAdult1 = edadThis =>
+            {
+                if (age >= 18)
+                {
+                    return "Is Adult!";
+                }
+                else
+                {
+                    return "Isn't Adult!";
+                }
+            };
+
+            Func<int, string> isAdult2 = edadThis => (age >= 18) ? "Is Adult!" : "Isn't Adult!";
+
+            Console.WriteLine(isAdult1(age)+"\n");
+            ////////////////////////////////////////////////////////////////////////////////////////////////
+
+            Console.WriteLine("================ Data Tables ==================");
+            TestingDataTables test = new TestingDataTables();
+
+            test.testingDataTables();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sandbox/Testing2/Testing2/TestingDataTables.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing2
+{
+    public class TestingDataTables
+    {
+        public void testingDataTables() {
+            // Crear una nueva DataTable
+            DataTable dataTable = new DataTable("EjemploTabla");            
+
+            // Definir las columnas (Encabezados)
+            dataTable.Columns.Add("ID", typeof(int));
+            dataTable.Columns.Add("Nombre", typeof(string));
+            dataTable.Columns.Add("Edad", typeof(int));
+
+            // Agregar filas a la DataTable (Contenido)
+            dataTable.Rows.Add(1, "Juan", 30);
+            dataTable.Rows.Add(2, "María", 25);
+            dataTable.Rows.Add(3, "Carlos", 35);
+
+            // Iterar a través de las filas y columnas de la DataTable
+            foreach (DataRow row in dataTable.Rows)
+            {
+                Console.WriteLine($"ID: {row["ID"]}, Nombre: {row["Nombre"]}, Edad: {row["Edad"]}");
+            }
+
+            Console.Write("Columnas: ");
+            foreach (DataColumn column in dataTable.Columns)
+            {
+                Console.Write($"{column} ");
+            }
+
+            // Realizar una consulta utilizando LINQ to DataSet
+            var consulta = from fila in dataTable.AsEnumerable()
+                           where fila.Field<int>("Edad") > 30
+                           select fila;
+
+            Console.WriteLine("\n\nPersonas mayores de 30 años:");
+            foreach (var resultado in consulta)
+            {
+                Console.WriteLine($"ID: {resultado["ID"]}, Nombre: {resultado["Nombre"]}, Edad: {resultado["Edad"]}");
+            }
+            
+            //Esta comparacion es sensible a mayusculas y minusculas
+            //la variable a se crea es tipo var y es temporal.
+            var consulta2 = from a in dataTable.AsEnumerable()
+                            where a.Field<string>("Nombre") == "Carlos"
+                            select new 
+                            { 
+                                Edad = a.Field<int>("Edad"), 
+                                Nombre = a.Field<string>("Nombre")
+                            };
+  
+            foreach (var resultado2 in consulta2)
+            {
+                Console.WriteLine($"Nombre: {resultado2.Nombre}, Edad: {resultado2.Edad}");                
+                //Console.WriteLine($"ID: {resultado["ID"]}, Nombre: {resultado["Nombre"]}, Edad: {resultado["Edad"]}");
+            }
+
+            //var consulta = from fila in dataTable.AsEnumerable()
+            //               where fila.Field<int>("Edad") > 30
+            //               select new
+            //               {
+            //                   Nombre = fila.Field<string>("Nombre"),
+            //                   Edad = fila.Field<int>("Edad")
+            //               };
+        }
+    }
+}
