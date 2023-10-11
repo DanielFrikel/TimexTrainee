12ebad948a68c1d651a2b5b394d6188f3638be59
Aguilar Daniel <124215580+DanielFrikel@users.noreply.github.com>
2023-10-11 11:54:37 -0700

    Add files via upload

--- AppMenuDaniel/AppMenuDaniel/Games/Calculator.cs
+++ AppMenuDaniel/AppMenuDaniel/Games/Calculator.cs
@@ -13,22 +13,28 @@ namespace AppMenuDaniel
         private static bool validNum;
         private static string wannaGoMain;
         public static void startCalculator() {
+            char[] options = new char[] { '+','-','/','*'};
             Console.Clear();
             validNum = true;
             //Greetings
             Console.WriteLine("====== Welcome to Basic Calculator ======");
-
+            
             //Start Calculator
             do { 
                 try
                 {
-                    Console.Write("Enter a number: ");
+                    Console.Write("Enter the first number: ");
                     num1 = Convert.ToDouble(Console.ReadLine());
 
                     Console.Write("Enter Operator: ");
                     op = Convert.ToChar(Console.ReadLine());
-    
-                    Console.Write("Enter a number: ");
+                    if (!options.Contains(op))
+                    {
+                        Console.WriteLine("\nError, ingrese un operador correcto. [+,-,*,/]");
+                        continue;
+                    }
+
+                    Console.Write("Enter the second number: ");
                     num2 = Convert.ToDouble(Console.ReadLine());
                         
                     Console.WriteLine($"\nYour result is: {startOperation(op)}!");
