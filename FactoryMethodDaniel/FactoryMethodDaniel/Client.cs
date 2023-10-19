13f0dc817db7b972a645dce3e80c38beec16afeb
Aguilar Daniel <124215580+DanielFrikel@users.noreply.github.com>
2023-10-19 11:50:49 -0700

    Add files via upload

---
+++ FactoryMethodDaniel/FactoryMethodDaniel/Client.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodDaniel
+{
+    public class Client
+    {
+        public void Main()
+        {
+            Console.WriteLine("App: Launched with the ConcreteCreator1.");
+            ClientCode(new ConcreteCreator1());
+
+            Console.WriteLine("");
+
+            Console.WriteLine("sApp: Launched with the ConcreteCreator2.");
+            ClientCode(new ConcreteCreator2());
+        }
+
+        public void ClientCode(Creator creator)
+        {
+            Console.WriteLine("Client: I'm not aware of the creator's class," +
+                "but it still works.\n" + creator.SomeOperation());
+        }
+    }
+}
---
+++ FactoryMethodDaniel/FactoryMethodDaniel/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodDaniel
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            new Client().Main();
+
+            Console.ReadKey();
+        }
+    }
+}
